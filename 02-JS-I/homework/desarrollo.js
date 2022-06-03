54e2ecd40d0fb27131901d7995950b9def32a923
unknown <facumedina0411@gmail.com>
2022-06-03 00:02:49 -0300

    JSI HW COMPLETADO

---
+++ 02-JS-I/homework/desarrollo.js
@@ -0,0 +1,12 @@
+/* Variables
+Es donde se almacena datos en la memoria para despues ser utilizados.
+Instrumentos de cuerda
+son bloques de texto, se definen entre comillas o doble comillas.
+Funciones (argumentos, return)
+Las funciones ejecutan un bloque de codigos, que puede tener argumentos, donde puede devolver o no un resultado.
+Declaracionesif
+son operaciones de desicion o de control, evaluan una expresion y devuelven un valor ya sea true o false.
+Valores booleanos ( true, false) 
+son operaciones que te devuelen un true o un false, tiene 2 opciones.
+
+*/
